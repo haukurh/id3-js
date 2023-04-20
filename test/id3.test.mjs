e945d4fe4b135797f1fcf7cce346068821ccfbc3
Haukur Hlöðversson <haukur@haukurh.is>
2023-04-20 16:40:36 +1000

    Added Jest config and minimal test

---
+++ test/id3.test.mjs
@@ -0,0 +1,8 @@
+'use strict';
+
+import { readID3 } from '../src/id3.mjs';
+
+test('readID3 should return an object', () => {
+	const output = readID3([]);
+	expect(output instanceof Object).toBe(true);
+});
