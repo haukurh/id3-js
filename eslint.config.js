01d224b16f85c9bfac4cc487e328e29103466614
Haukur Hlöðversson <haukur@haukurh.is>
2023-04-20 17:47:21 +1000

    Added eslint

---
+++ eslint.config.js
@@ -0,0 +1,22 @@
+export default [
+	{
+		rules: {
+			'indent': [
+				'error',
+				'tab'
+			],
+			'linebreak-style': [
+				'error',
+				'unix'
+			],
+			'quotes': [
+				'error',
+				'single'
+			],
+			'semi': [
+				'error',
+				'always'
+			]
+		}
+	}
+];
