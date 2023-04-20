2b764473cc3b307d857e9ab672f10eb524489818
Haukur Hlöðversson <haukur@haukurh.is>
2023-04-20 18:27:37 +1000

    [eslint] Added eslint recommended config

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,12 @@
+import js from '@eslint/js';
+
 export default [
+	js.configs.recommended,
 	{
 		files: ['**/*.mjs', '**/*.js'],
 		rules: {
+			'strict': 'error',
+			'no-unused-vars': 'error',
 			'indent': [
 				'error',
 				'tab'
