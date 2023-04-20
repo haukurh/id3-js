f1de50d18f898e3a48204d490002f81c414ab0e7
Haukur Hlöðversson <haukur@haukurh.is>
2023-04-20 18:40:19 +1000

    [eslint] refactor eslint config, added custom rules + fix for tests

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,44 @@
 import js from '@eslint/js';
 
+// Our custom rules for eslint which we overwrite if needed
+const rules = {
+	'strict': 'error',
+	'no-undef': 'error',
+	'no-unused-vars': 'error',
+	'indent': [
+		'error',
+		'tab'
+	],
+	'linebreak-style': [
+		'error',
+		'unix'
+	],
+	'quotes': [
+		'error',
+		'single'
+	],
+	'semi': [
+		'error',
+		'always'
+	],
+};
+
 export default [
-	js.configs.recommended,
 	{
+		...js.configs.recommended,
 		files: ['**/*.mjs', '**/*.js'],
 		rules: {
-			'strict': 'error',
-			'no-unused-vars': 'error',
-			'indent': [
-				'error',
-				'tab'
-			],
-			'linebreak-style': [
-				'error',
-				'unix'
-			],
-			'quotes': [
-				'error',
-				'single'
-			],
-			'semi': [
-				'error',
-				'always'
-			]
+			...js.configs.recommended.rules,
+			...rules,
+		}
+	},
+	{
+		...js.configs.recommended,
+		files: ['test/**/*.mjs'],
+		rules: {
+			...js.configs.recommended.rules,
+			...rules,
+			'no-undef': 'off',
 		}
 	}
 ];
