9732fe6ebf022baa837e6c9b8a4804b9e46645b0
Haukur Hlöðversson <haukur@haukurh.is>
2023-04-21 20:55:28 +1000

    Refactor: clean up eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,44 +1,30 @@
 import js from '@eslint/js';
 
-// Our custom rules for eslint which we overwrite if needed
-const rules = {
-	'strict': 'error',
-	'no-undef': 'error',
-	'no-unused-vars': 'error',
-	'indent': [
-		'error',
-		'tab'
-	],
-	'linebreak-style': [
-		'error',
-		'unix'
-	],
-	'quotes': [
-		'error',
-		'single'
-	],
-	'semi': [
-		'error',
-		'always'
-	],
-};
-
 export default [
 	{
 		...js.configs.recommended,
 		files: ['**/*.mjs', '**/*.js'],
 		rules: {
 			...js.configs.recommended.rules,
-			...rules,
+			'strict': 'error',
+			'no-undef': 'error',
+			'no-unused-vars': 'error',
+			'indent': [
+				'error',
+				'tab'
+			],
+			'linebreak-style': [
+				'error',
+				'unix'
+			],
+			'quotes': [
+				'error',
+				'single'
+			],
+			'semi': [
+				'error',
+				'always'
+			],
 		}
 	},
-	{
-		...js.configs.recommended,
-		files: ['test/**/*.mjs'],
-		rules: {
-			...js.configs.recommended.rules,
-			...rules,
-			'no-undef': 'off',
-		}
-	}
 ];
