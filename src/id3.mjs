ce84af462dbefff4ed9839638118053dccb3eea4
Haukur Hlöðversson <haukur@haukurh.is>
2023-04-21 10:32:50 +1000

    Added ability to read ID3v1 and ID3v2
    Added tests for all available tags

--- src/id3.mjs
+++ src/id3.mjs
@@ -1,5 +1,44 @@
 
+import {
+	readChars,
+	getUInt8,
+	getUInt32,
+} from './binary.mjs';
+
+const readIDv1 = (file) => {
+	// in ID3v1 it's always the last 128 bytes of the file
+	const tag = file.slice(-128);
+
+	// ID3v1 should always start with TAG
+	if (readChars(tag, 0, 3) !== 'TAG') {
+		return null;
+	}
+	return {
+		version: 'ID3v1',
+	};
+};
+
+const readID3v2 = (file) => {
+	const id3Tag = readChars(file, 0, 3);
+
+	if (id3Tag !== 'ID3') {
+		return null;
+	}
+
+	const minor = getUInt8(file, 3);
+	const patch = getUInt8(file, 4);
+
+	return {
+		version: `${id3Tag}v2.${minor}.${patch}`,
+		size: getUInt32(file, 6),
+	};
+};
 
 export const readID3 = (file) => {
-	return {};
+
+	const id3v2 = readID3v2(file);
+	if (id3v2) {
+		return id3v2;
+	}
+	return readIDv1(file);
 };
--- test/id3.test.mjs
+++ test/id3.test.mjs
@@ -1,8 +1,27 @@
-
-
+import * as fs from 'fs';
 import { readID3 } from '../src/id3.mjs';
 
-test('readID3 should return an object', () => {
-	const output = readID3([]);
-	expect(output instanceof Object).toBe(true);
+const id3v1_file = fs.readFileSync('./test/files/id3v1.mp3');
+const id3v1v2_file = fs.readFileSync('./test/files/id3v1v2.mp3');
+const id3v23_file = fs.readFileSync('./test/files/id3v2.3.mp3');
+const id3v24_file = fs.readFileSync('./test/files/id3v2.4.mp3');
+const noMeta_file = fs.readFileSync('./test/files/no-meta.mp3');
+
+test('should be able to read ID3v2', () => {
+	const id3v1v2 = readID3(new Uint8Array(id3v1v2_file.buffer));
+	const id3v23 = readID3(new Uint8Array(id3v23_file.buffer));
+	const id3v24 = readID3(new Uint8Array(id3v24_file.buffer));
+	expect(id3v24.version).toBe('ID3v2.4.0');
+	expect(id3v23.version).toBe('ID3v2.3.0');
+	expect(id3v1v2.version).toBe('ID3v2.3.0');
+});
+
+test('should be able to read ID3v1', () => {
+	const id3v1 = readID3(new Uint8Array(id3v1_file.buffer));
+	expect(id3v1.version).toBe('ID3v1');
+});
+
+test('should return null if no ID3 tag found', () => {
+	const noMeta = readID3(new Uint8Array(noMeta_file.buffer));
+	expect(noMeta).toBeNull();
 });
