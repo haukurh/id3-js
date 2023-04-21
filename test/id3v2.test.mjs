94353a78a33630b05d1dc49dfecd0e1bb1c831e2
Haukur Hlöðversson <haukur@haukurh.is>
2023-04-22 00:28:25 +1000

    Test for fooerPresent flag in ID3v2

--- test/id3v2.test.mjs
+++ test/id3v2.test.mjs
@@ -10,12 +10,15 @@ test('should parse ID3v2 flags correctly', () => {
 	expect(id3v1v2.flags.unsynchronisation).toBe(false);
 	expect(id3v1v2.flags.extendedHeader).toBe(false);
 	expect(id3v1v2.flags.experimentalIndicator).toBe(false);
+	expect(id3v1v2.flags.footerPresent).toBe(false);
 
 	expect(id3v23.flags.unsynchronisation).toBe(false);
 	expect(id3v23.flags.extendedHeader).toBe(false);
 	expect(id3v23.flags.experimentalIndicator).toBe(false);
+	expect(id3v23.flags.footerPresent).toBe(false);
 
 	expect(id3v24.flags.unsynchronisation).toBe(false);
 	expect(id3v24.flags.extendedHeader).toBe(true);
 	expect(id3v24.flags.experimentalIndicator).toBe(false);
+	expect(id3v24.flags.footerPresent).toBe(false);
 });
